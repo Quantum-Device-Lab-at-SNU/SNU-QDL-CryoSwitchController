d41d0cf5aeae3ab5c6172a0c7d95ead562a1f404
cristobal@qphox.eu <cristobal@qphox.eu>
2022-09-22 11:22:31 +0200

    Added UI for running from cmd

--- radiall_switch_controller.py
+++ radiall_switch_controller.py
@@ -8,7 +8,7 @@ import logging
 import labjack.ljm.ljm as ljm
 
 
-class radial_switch_controller:
+class radiall_switch_controller:
 
     def __init__(self, labjack_instrument=None, labjack_pinnames=None):
 
@@ -35,13 +35,7 @@ class radial_switch_controller:
         for port in range(1, 7):
             self.lj.write_name(self.lj_pinnames['line_' + str(port)], 0)
 
-    def check_before_switching(self, port):
-        if port < 1 or port > 6:
-            raise ValueError(f'port number must be between 1-6, not {port}')
-        cur_time = time.time()
-        if cur_time - self.last_switch_time < self.minimum_time_between_switches:
-            raise Exception(
-                f'time since last switch ({cur_time - self.last_switch_time:.1f} s) is less than minimum_time_between_switches ({self.minimum_time_between_switches:.0f} s)')
+        self.debug = True
 
     def connect_switch_port(self, port):
         port = int(port)
@@ -53,11 +47,22 @@ class radial_switch_controller:
         self.check_before_switching(port)
         self._do_switch_raw(port, 0)
 
-    def read_switch_state(self):
+    def read_switch_state(self, fig=False):
         switch_state = {}
         for k, v in self.lj_pinnames.items():
             switch_state[k] = int(self.lj.read_dio_state(v))
+
+        if fig:
+            self.print_switch_state(switch_state)
         return switch_state
+
+    def check_before_switching(self, port):
+        if port < 1 or port > 6:
+            raise ValueError(f'ERROR: Port number must be between 1-6, not {port}')
+        cur_time = time.time()
+        if cur_time - self.last_switch_time < self.minimum_time_between_switches:
+            raise Exception(
+                f'ERROR: Time since last switch ({cur_time - self.last_switch_time:.1f} s) is less than minimum_time_between_switches ({self.minimum_time_between_switches:.0f} s)')
 
     def _do_switch_raw(self, port, val):
         lj_pin_name = self.lj_pinnames['line_' + str(port)]
@@ -65,13 +70,30 @@ class radial_switch_controller:
         if cur_pin_val == val:
             raise Exception(
                 'Current labjack pin value is same as target value, i.e the switch is already in target state, or there is a labjack vs swich state mismatch')
-        print(
-            f'current lj pin {lj_pin_name} value = {cur_pin_val}, switching it to {val}')
 
-        print('switching now')
+        if self.debug:
+            print(f'Current lj pin {lj_pin_name} value = {cur_pin_val}, switching it to {val}')
+
+        print('Switching now')
         self.lj.write_name(lj_pin_name, val)
         self.last_switch_time = time.time()
-        print('switching done')
+        print('Switching done')
+        print('')
+
+    def print_switch_state(self, sw_state):
+        print('')
+        print('Switch state')
+        for switch in range(1, len(self.lj_pinnames) + 1):
+            state = sw_state['line_' + str(switch)]
+            if state:
+                if switch == 1:
+                    print(str(switch) + ' ----' + chr(0x2510))
+                else:
+                    print(str(switch) + ' ----' + chr(0x2524))
+            else:
+                print(str(switch) + ' -  -' + chr(0x2502))
+        print('      ' + chr(0x2514) + '- IN')
+        print('')
 
 
 class LJ:
@@ -147,18 +169,11 @@ class LJ:
 
 if __name__ == '__main__':
 
-    # example of usage
 
-    controller = radial_switch_controller()
-    i = 6
-    for i in range(1, 7):
-        time.sleep(0.5)
-        controller.connect_switch_port(i)
-        time.sleep(0.5)
-        controller.disconnect_switch_port(i)
+    # Usage Example
 
-    for i in range(1, 7):
-        time.sleep(1)
-        controller.connect_switch_port(i)
-        time.sleep(0.48)
-        controller.disconnect_switch_port(i)
+    controller = radiall_switch_controller()  # Create a switch controller instance
+    controller.connect_switch_port(1)  # Connects RF input 1 from the common RF terminal
+    controller.read_switch_state()  # prints the current switch state
+    time.sleep(35)  # Wait at least 30s before the next switching to avoid excessive heating
+    controller.disconnect_switch_port(1)  # Disconnect RF input 1 from the common RF terminal
